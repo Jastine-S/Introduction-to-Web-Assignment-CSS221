f9fac6a446661760a2ed526e93360dd1f7ae9914
Jastine-S <sangetijustine@gmail.com>
2026-06-08 21:27:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("Welcome to my portfolio website");
+console.log("JS is connected");
+
+let lastResult = 0;
+
 function calculate(op) {
-    let n1 = parseFloat(document.getElementById("num1").value);
-    let n2 = parseFloat(document.getElementById("num2").value);
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+
     let result;
 
     if (op === '+') {
@@ -15,7 +20,12 @@ function calculate(op) {
     } 
     else if (op === '/') {
         result = n1 / n2;
+    } 
+    else if (op === '=') {
+        result = lastResult;
     }
 
-    document.getElementById("result").innerText = "Result: " + result;
+    lastResult = result;
+
+    document.getElementById("result").textContent = "Result: " + result;
 }
