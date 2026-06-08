2c0b5b8e4909a2d589c4069053063f119e5f0015
Jastine-S <sangetijustine@gmail.com>
2026-06-08 20:58:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Welcome to my portfolio website");
+function calculate(op) {
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let n2 = parseFloat(document.getElementById("num2").value);
+    let result;
+
+    if (op === '+') {
+        result = n1 + n2;
+    } 
+    else if (op === '-') {
+        result = n1 - n2;
+    } 
+    else if (op === '*') {
+        result = n1 * n2;
+    } 
+    else if (op === '/') {
+        result = n1 / n2;
+    }
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
